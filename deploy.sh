269d8685040ca79eb651f02fb7165375306ef4b1
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-03-20 16:40:07 +0100

    Final updates to deploy script. Now for testing

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # To execute run ./deploy.sh
+# Must be within the dsn-leaf directory
 
+# General updates
 apt update
 apt upgrade
 
+# Install docker and Docker-compose
 apt install docker.io
 apt install docker-compose
 
-docker-compose up
+# Clone the dsn-leaf repo
+git clone https://github.com/trav-d13/dsn-leaf.git
+
+# Navigate into directory and update
+cd dsn-leaf
+git pull
+
+docker-compose build # Perform build in case any missed changes
+docker-compose up  # Execute leaf functionality
